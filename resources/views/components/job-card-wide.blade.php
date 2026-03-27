5b381a3c79dca8c42365d97eb08a08803071db8a
FaithMutua-code <charlesfaith157@gmail.com>
2026-03-27 15:44:34 +0300

    blade components for job,panel,tags etc

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,22 +1,23 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-    <img src="https://placehold.co/42x42/000000/FFFFFF" alt="placeholder">
-       <div class="">
-          <a href="#" class="self-start text-sm">Fai</a>
-        </div>
-        <div class="py-8 font-bold">
-            <h3 class="mb-6">title</h3>
-            <p>description</p>
-        </div>
+<x-panel class="flex gap-x-6">
+   <div class="">
+  <x-employer-logo />
        </div>
-        <div class="flex justify-between items-center mt-auto">
-           <div class="">
+       <div class="flex-1 flex flex-col">
+          <a href="#" class="self-start text-sm">Fai</a>
+            
+            <h3 class=" font-bold text-xl mt-3 transition-colors duration-300 group-hover:text-indigo-600">title</h3>
+            <p class="text-sm text-gray-400 mt-auto">description</p>
+        </div>
+       <div class="">
               <x-tag>Tag</x-tag>
               <x-tag>Tag</x-tag>
               <x-tag>Tag</x-tag>
            </div>
+      </x-panel>
+       
+      
+         
           
           
 
-           
-        </div>
-    </div>
+         
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,18 +1,24 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-        <div class="self-start">Fai</div>
-        <div class="py-8 font-bold">
-            <h3 class="mb-6">title</h3>
-            <p>description</p>
-        </div>
-        <div class="flex justify-between items-center mt-auto">
-           <div class="">
-              <x-tag>Tag</x-tag>
-              <x-tag>Tag</x-tag>
-              <x-tag>Tag</x-tag>
-           </div>
-          
-           <img src="https://placehold.co/42x42/000000/FFFFFF" alt="placeholder">
+<x-panel class="flex flex-col text-center group h-full">
+        
+    <div class="text-left">Fai</div>
 
-           
-        </div>
+    <div class="py-8 flex-1">
+        <h3 class="mb-4 text-xl font-bold group-hover:text-indigo-600 transition-colors duration-300">
+            title
+        </h3>
+        <p class="text-sm text-gray-400 mt-2">
+            description
+        </p>
     </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div class="flex gap-2 flex-wrap">
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
+        </div>
+
+        <x-employer-logo :width="42" />
+    </div>
+
+</x-panel>
